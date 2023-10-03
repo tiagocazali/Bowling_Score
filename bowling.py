4c57e6e33223ba6e768bd4a34b06150e9402cb4e
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2023-10-03 22:44:54 +0200

    improvements in calculate_frame_point

--- bowling.py
+++ bowling.py
@@ -8,10 +8,12 @@ class Bowling:
         self.frame_table.append([first_ball, second_ball])
 
     #This function will calculate ALL scores, It is a rest
-    def full_calculate_point(self):
+    def full_calculate_point(self,is_full_calculation=False):
         
-        self.score_table.clear() #ERASE all the calculated points and start from zero
+        if not is_full_calculation: 
+            pass
 
+        self.score_table.clear() #ERASE all the calculated points and start from zero
         for frame, points in enumerate(self.frame_table):
         
             #STRIKE
@@ -82,59 +84,44 @@ class Bowling:
         #STRIKE
         if points[0] == 10:
             self.score_table.append(None)
-
-            #Check if it is a TRIPLE STRIKE
-            if frame >=2: #It is possible only after the 3 frame
-                if (self.frame_table[frame-1][0] == 10) and (self.frame_table[frame-2][0] == 10): #it is waiting extra points
-                    self.score_table[frame-2] = 30
-
-            #Check if previous frame is waiting for extra points
-            if frame >=1: #You can NOT check the last frame if it is the Fisrt frame
-                if self.score_table[frame-1] is None:
-                    
-                    if self.frame_table[frame-1][0] == 10: #check if the last frame was a Strike
-                        pass
-                    else:
-                        self.score_table[frame-1] = 20
-
-
+        
         #SPERE
         elif sum(points) == 10:
             self.score_table.append(None)
 
-            #Check if previous frame is waiting for extra points
-            if frame >=1: #You can NOT check the last frame if it is the Fisrt frame
-                if self.score_table[frame-1] is None:
-                    
-                    if self.frame_table[frame-1][0] == 10: #check if the last frame was a Strike
-                        self.score_table[frame-1] = 20
-                    else:
-                        self.score_table[frame-1] = 10 + points[0]
-                    
-                    #Check if 2 frames ago is waiting for extra points - Duble Strike - (x)(x)(9,1)
-                    if frame >=2: #It is possible only after the 3 frame
-                        if self.score_table[frame-2] is None:
-                            self.score_table[frame-2] = 10 + 10 + points[0]
-
-
         #NORMAL frame - No strike and NO spere
         else:
             self.score_table.append(sum(points))
+        
 
-            #Check if previous frame is waiting for extra points
-            if frame >=1: #You can NOT check the last frame if it is the Fisrt frame
+        #Check if previous frame is waiting for extra points
+        if frame >=1: #You can NOT check the last frame IF it is the Fisrt frame
+            if self.score_table[frame-1] is None:
                 
-                if self.score_table[frame-1] is None: #it is waiting extra points
+                #Last frame is Strike
+                if self.frame_table[frame-1][0] == 10: 
                     
-                    if self.frame_table[frame-1][0] == 10: #check if the last frame was a Strike
-                        self.score_table[frame-1] = 10 + sum(points)
+                    #Actual Frame is also Strike - (x)(x)
+                    if points[0] == 10: 
+                        
+                        #Check if it is a TRIPLE STRIKE - (x)(x)(x)
+                        if frame>=2:  #It is possible only after the 3 frame
+                            if self.score_table[frame-2] is None:
+                                self.score_table[frame-2] = 30
+                        
+                        #It is only Duble Strike - (x)(x)
+                        else:
+                            pass #It continue to be None - (none)(none)
+
+                    #Actual frame is a Normal frame or Spare (x)(9,1)
                     else:
+                        self.score_table[frame-1] = 10 + sum(points)
+
+                #Last frame is Spare
+                else:
                         self.score_table[frame-1] = 10 + points[0]
-                    
-                    #Check if 2 frames ago is waiting for extra points - Duble Strike - (x)(x)(3,4)
-                    if frame >=2: #It is possible only after the 3 frame
-                        if self.score_table[frame-2] is None:
-                            self.score_table[frame-2] = 10 + 10 + points[0]
+        
+
 
 
 
@@ -164,9 +151,9 @@ player1.show_frame_table()
 player1.show_score_table()
 player1.show_total_score()
 
-player1.full_calculate_point()
-player1.show_score_table()
-player1.show_total_score()
+#player1.full_calculate_point()
+#player1.show_score_table()
+#player1.show_total_score()
 
 
 
