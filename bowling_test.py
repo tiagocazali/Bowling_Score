b866a5da74b6823fdb73cf96cb7f53e3508f1bad
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2023-10-08 00:48:23 +0200

    error in XX/ - Starting corrections

--- bowling_test.py
+++ bowling_test.py
@@ -1,8 +1,11 @@
 import pytest
 from bowling import Bowling
 
-frame_test = [[9,1],[10],[9,1],[10],[9,0],[4,4],[10],[2,6],[10,0],[6,4,8]]
-score_test = [20, 20, 20, 19, 9, 8, 18, 8, 20, 18]
+frame_test = [[10,0],[10,0],[10,0],[7,3],[8,1],[9,0],[9,1],[10,0],[7,0],[8,2,9]]
+score_test = [  30,    27,   20,   18,   9,    9,    20,   17,    7,    19]
+
+frame_test2 = [[9,1],[10],[9,1],[10],[9,0],[4,4],[10],[2,6],[10,0],[6,4,8]]
+score_test2 = [20, 20, 20, 19, 9, 8, 18, 8, 20, 18]
 
 def test_first_frame_spere():
     player1 = Bowling()
@@ -31,6 +34,18 @@ def test_triple_Strike():
     player1.calculate_points(True)
 
     assert player1.score_table[1] == 30
+
+def test_duble_Strike():
+    player1 = Bowling()
+
+    player1.add_points(6,3)
+    player1.add_points(10)
+    player1.add_points(10)
+    player1.add_points(3,3)
+    player1.add_points(3,4)
+    player1.calculate_points(True)
+
+    assert player1.score_table[1] == 23
 
 def test_full_play():
     
